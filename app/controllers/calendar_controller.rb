ddd4e31664abe72d8ab60c582af092da12eed202
Pedro <pedro@bydlowski.com>
2016-04-28 09:32:30 -0300

    Calendar code

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,7 +1,7 @@
 class CalendarController < ApplicationController
   def index
     @time = Time.now
-    @month = 4
+    @month = 2
     @year = 2016
   end
 end
--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -3,6 +3,8 @@ module CalendarHelper
   def calendar_method(month, year)
     current_date = Date.new(year, month, 1)
     day_of_the_week = current_date.strftime("%w").to_i
+    the_day = 0
+    month_days = Time.days_in_month(month, year)
     
     cal = "<table border='1'><tr>"
     days = %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
@@ -15,13 +17,58 @@ module CalendarHelper
       cal += "<td>&nbsp;</td>"
     end
     # First days of the month (first row)
-    (7 - day_of_the_week).times do |day|
-      cal += "<td>#{day + 1}</td>"
+    (7 - day_of_the_week).times do
+      cal += "<td>#{the_day += 1}</td>"
     end
     cal += "</tr>"
+    # Second row
+    cal += "<tr>"
+    7.times do 
+      cal += "<td>#{the_day += 1}</td>"
+    end
+    cal += "</tr>"
+    # Third row
+    cal += "<tr>"
+    7.times do 
+      cal += "<td>#{the_day += 1}</td>"
+    end
+    cal += "</tr>"
+    # Forth row
+    cal += "<tr>"
+    7.times do
+      cal += "<td>#{the_day += 1}</td>"
+    end
+    cal += "</tr>"
+    # Fifth row (if necessary)
+    if the_day < month_days
+      cal += "<tr>"
+      7.times do
+        if the_day < month_days
+          cal += "<td>#{the_day += 1}</td>"
+        else 
+          the_day += 1
+          cal += "<td>&nbsp;</td>"
+        end
+      end
+      cal += "</tr>"
+    end
+    # Sixth row (if necessary)
+    if the_day < month_days
+      cal += "<tr>"
+      7.times do
+        if the_day < month_days
+          cal += "<td>#{the_day += 1}</td>"
+        else 
+          the_day += 1
+          cal += "<td>&nbsp;</td>"
+        end
+      end
+      cal += "</tr>"
+    end
     cal += "</tr></table>"
-    cal += "Month: #{month}<br />Year: #{year}<br />Date: #{current_date}"
+    cal += "Month: #{month}<br />Year: #{year}<br />Date: #{current_date}<br />Days in the month: #{month_days}"
     return cal
   end
 
 end
+
