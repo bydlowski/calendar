0a0716564789411220fc28a1983898a63e6eca12
Pedro <pedro@bydlowski.com>
2016-05-13 12:49:35 -0700

    Year review coding

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -513,17 +513,43 @@ module CalendarHelper
 
     y = all_holidays_method(year, holiday)
 
-    #all_holidays_num = x[4]
-    #all_holidays_text = x[5]
+    all_holidays_num = y[4]
+    all_holidays_text = y[5]
     all_holidays_dates = y[6]
-    #all_holidays_day_of_week = x[7]
-    
+    all_holidays_day_of_week = y[7]
+
+    # How many long holidays
+    two_day_hol = 0
+    two_day_hol_info = []
+    all_holidays_day_of_week.each_with_index do |index, element|
+      if (element == 2 || element == 4)
+        two_day_hol_info << "#{all_holidays_text[index]} - #{(all_holidays_dates[index]).strftime('%d/%m')}"
+        two_day_hol += 1
+      end
+    end
+
+    # Holiday text
+    two_hol_length = two_day_hol_info.length
+    if two_hol_length == 0
+      two_day_hol_text = "Não teremos feriados de dois dias nesse ano :("
+    elsif two_hol_length == 1
+      two_day_hol_text = "#{two_day_hol_info[0]}"
+    elsif two_hol_length == 2
+      two_day_hol_text = "#{two_day_hol_info[0]} e #{two_day_hol_info[1]}"
+    elsif two_hol_length == 3
+      two_day_hol_text = "#{two_day_hol_info[0]}, #{two_day_hol_info[1]} e #{two_day_hol_info[2]}"
+    end
+   
     div_year = ""
-    div_year += "<h3>Year: #{year}</h3>"
+    div_year += ""
     div_year += "<div>"
+    div_year += "<h3>#{year} facts</h3>"
+    div_year += "<p>Número de feriados prolongados: #{two_day_hol}</p>"
+    div_year += "<p>Quais são os feriados prolongados: #{two_day_hol_text}</p>"
     div_year += "#{all_holidays_dates}<br />"
 
     div_year += "</div>"
+    div_year += "#{two_day_hol_info}<br />"
 
     return div_year
   end
